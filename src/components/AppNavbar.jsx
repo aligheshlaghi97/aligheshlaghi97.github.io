dcd5ae9f5a99350428160f14b544d164de6f9aaf
ali gheshlaghi <aliigheshlaghi97@gmail.com>
2024-02-23 22:19:31 +0330

    Added cdnjs font-awesome, utilized it in Navbar, debuged Navbar to be shown in small screens and have dropdown format.

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import "../styles/AppNavbar.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const AppNavbar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="md" className="custom-navbar" sticky='top'>
-      <Navbar.Collapse id="navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home" className="nav-btn">Home</Nav.Link>
-          <Nav.Link href="#about" className="nav-btn">About</Nav.Link>
-          <Nav.Link href="#projects" className="nav-btn">Projects</Nav.Link>
-          <Nav.Link href="#contact" className="nav-btn">Contact</Nav.Link>
-          <Nav.Link href="#resume" className="nav-btn">Resume</Nav.Link>
-          <Nav.Link href="#testimonials" className="nav-btn">Testimonials</Nav.Link>
+      <Navbar bg="dark" variant="dark" expand="lg" className="custom-navbar" sticky='top'>
+      <Container>
+        <Navbar.Brand href="#home">Personal-Website</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+          <Nav.Link href="#home" className="nav-btn"><i className="fa-solid fa-house" /> Home</Nav.Link>
+          <Nav.Link href="#about" className="nav-btn"><i className="fa-regular fa-address-card" /> About</Nav.Link>
+          <Nav.Link href="#projects" className="nav-btn"><i className="fa-solid fa-bars-progress" /> Projects</Nav.Link>
+          <Nav.Link href="#contact" className="nav-btn"><i className="fa-regular fa-address-book" /> Contact</Nav.Link>
+          <Nav.Link href="#resume" className="nav-btn"><i className="fa-regular fa-file" /> Resume</Nav.Link>
+          <Nav.Link href="#testimonials" className="nav-btn"><i className="fa-solid fa-people-arrows" /> Testimonials</Nav.Link>
         </Nav>
       </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
