c0aa1469d1b0155a9be3dcec9cff7453a46d1f33
ali gheshlaghi <aliigheshlaghi97@gmail.com>
2024-02-25 04:55:47 +0330

    Added better style into about section.

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -24,51 +24,51 @@ const AboutSection = () => {
 
 
   return (
-      <Container className={`${isVisible ? 'animate-fade-in' : ''}`}
-                   ref={textRef} id="about">
-        <Row>
-          <div className="container-header-about-me">
-            <h2>About Me</h2>
-          </div>
-          <div className="container-about-me">
-            <h4>
-             I am a passionate quantitative developer/analyst with experience in Python, Django and FastAPI.
-             I love building web applications and exploring new technologies related to financial software solutions.
-            </h4>
-          </div>
-        </Row>
+    <Container className={`${isVisible ? 'animate-fade-in' : ''}`}
+               ref={textRef} id="about">
+      <Row>
+        <div className="container-header-about-me">
+          <h2>About Me</h2>
+        </div>
+        <div className="container-about-me">
+          <h4>
+           I am a passionate backend developer with experience in Python, Django and FastAPI.
+           I love building web applications and exploring new technologies related to web development and financial
+           software solutions. I also have two years of experience as quantitative developer/analyst.
+          </h4>
+        </div>
+      </Row>
 
-        <Row className="experience">
-          <h3 className="container-header-my-skills">My Skills</h3>
-          <div className="skills-rows">
-            <div className="circle-custom">
-             <div className="skills-list">
-             <h4 >Quant Developer</h4>
-             <ul className="skills-ul">
-               <li>Python</li>
-               <li>Django</li>
-               <li>PostgreSQL</li>
-               <li>JavaScript</li>
-               <li>React</li>
-             </ul>
-             </div>
-           </div>
-
+      <Row className="experience">
+        <h3 className="container-header-my-skills">My Skills</h3>
+        <div className="skills-rows">
           <div className="circle-custom">
-              <div className="skills-list">
-             <h4>Quant Analyst</h4>
+            <div className="skills-list">
+              <h4 >Quant Developer</h4>
               <ul className="skills-ul">
-               <li>Pandas</li>
-               <li>Backtest</li>
-               <li>SQL</li>
-               <li>Statistics</li>
-               <li>Tableau</li>
-             </ul>
-              </div>
+                <li>Python</li>
+                <li>Django</li>
+                <li>PostgreSQL</li>
+                <li>JavaScript</li>
+                <li>React</li>
+              </ul>
+            </div>
           </div>
+          <div className="circle-custom">
+            <div className="skills-list">
+              <h4>Quant Analyst</h4>
+              <ul className="skills-ul">
+                <li>Pandas</li>
+                <li>Backtest</li>
+                <li>SQL</li>
+                <li>Statistics</li>
+                <li>Tableau</li>
+              </ul>
+            </div>
           </div>
-        </Row>
-      </Container>
+        </div>
+      </Row>
+    </Container>
   );
 };
 
